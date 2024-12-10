601384db5260a0c305cdefebbc7fc0d86c07cf1e
Chiang Kian Seng <chiangkianseng@stigp.com>
2024-12-10 16:14:12 +0800

    [Update] Update look to include side bar

--- app.js
+++ app.js
@@ -1,35 +1,31 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const formTargetInfo = document.getElementById('TargetInfoForm');
+document.addEventListener('DOMContentLoaded', () => {
+    const formTargetInfo = document.getElementById('target-info-form');
 
-//     formTargetInfo.addEventListener('submit', (event) => {
-//         event.preventDefault();
-        
-//         const breakpointDist = document.getElementById('BreakpointDist').value;
-//         const breakpointBoard = document.getElementById('BreakpointBoard').value;
-//         const targetDist = document.getElementById("TargetDist").value;
-//         const targetBoard = document.getElementById('TargetBoard').value;
-        
-//         alert(`Target information\nBreakpoint Distance=${breakpointDist}\n` +
-//             `Breakpoint Board=${breakpointBoard}\n` + 
-//             `Target Distance=${targetDist}\n` + 
-//             `Target Board=${targetBoard}`);
-
-//     });
-// });
-
-$(document).ready(function() {
-    $('#TargetInfoForm').on('submit', function(event) {
+    formTargetInfo.addEventListener('submit', (event) => {
         event.preventDefault();
 
-        var breakpointDist = $('#BreakpointDist').val();
-        var breakpointBoard = $('#BreakpointBoard').val();
-        var targetDist = $('#TargetDist').val();
-        var targetBoard = $('#TargetBoard').val();
+        const breakpointDist = parseInt(document.getElementById('breakpoint-dist').value);
+        const breakpointBoard = parseInt(document.getElementById('breakpoint-board').value);
+        const targetDist = parseInt(document.getElementById('target-dist').value);
+        const targetBoard = parseInt(document.getElementById('target-board').value);
+        const personalNum = parseInt(document.getElementById('personal-number').value);
 
-        alert(`Target information\nBreakpoint Distance=${breakpointDist}\n` +
-            `Breakpoint Board=${breakpointBoard}\n` + 
-            `Target Distance=${targetDist}\n` + 
-            `Target Board=${targetBoard}`);
+        const ratio = targetDist / breakpointDist;
+        const breakBoards = targetBoard - breakpointBoard;
+        const ratioBoard = Math.round(ratio * breakBoards);
+        const foulLineBoard = targetBoard + ratioBoard;
+        const slideBoard = foulLineBoard + personalNum;
+        const standBoard = slideBoard + ratioBoard;
 
+        console.log(`1.ratio=${ratio}`);
+        console.log(`2.breakBoards=${breakBoards}`);
+        console.log(`3.ratioBoard=${ratioBoard}`);
+        console.log(`4.foulLineBoard=${foulLineBoard}`);
+        console.log(`5.slideBoard=${slideBoard}`);
+        console.log(`6.standBoard=${standBoard}`);
+
+        document.getElementById('foul-line').value = foulLineBoard;
+        document.getElementById('slide-at').value = slideBoard;
+        document.getElementById('stand-on').value = standBoard;
     });
 });
